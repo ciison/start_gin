365bc99dfa83b1eea55d21141826cd3b18f0cfb1
ciison <784457618@qq.com>
2020-05-19 12:30:11 +0800

    middlewares

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	
 	"github.com/gin-gonic/gin"
+	"start_gin/middlewares"
 )
 
 func main() {
-	gin.Default().Run()
+	engine := gin.Default()
+	
+	engine.Use(middlewares.MiddlewareCros) // 注册使用中间件
+	
+	engine.GET("/index", func(ctx *gin.Context) {
+		ctx.AbortWithStatusJSON(200, gin.H{"status": "ok"})
+	})
+	
+	err := engine.Run()
+	if err != nil {
+		panic(err)
+	}
 }
+
---
+++ middlewares/middlewares.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import (
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+)
+
+func MiddlewareCros(ctx *gin.Context) {
+	
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
+	
+	if http.MethodOptions == ctx.Request.Method {
+		ctx.JSON(http.StatusOK, "Options Request!")
+		return
+	}
+	
+	ctx.Next()
+	
+}
